401f31894e5ecc77acefcb5e5ecbb36b3dfe762d
Bradley_Carson <bmcarson1671@gmail.com>
2017-08-24 14:57:07 -0400

    building login, also need mongodb

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./router');
-//absolute path
+
+//passport dependencies ***
+const User = require('./models/user');
+const bcrype = require('bcryptjs');
+const mongoose = require('mongoose');
+const passport = require('passport'),
+      LocalStrategy = require('passport-local').Strategy,
+      session = require('express-session'),
+      flash = require('express-flash-messages');
+
+//absolute path for css
 const path = require('path');
 
 const app = express();
@@ -11,9 +21,46 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//middles
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//passport setup ***
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.authenticate(username, password, function(err, user) {
+            if (err) {
+                return done(err)
+            }
+            if (user) {
+                return done(null, user)
+            } else {
+                return done(null, false, {
+                    message: "There is no user with that username and password."
+                })
+            }
+        })
+    }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
 //passing app into routes
 routes(app);
 
 app.listen(3000);
-
-// module.exports = app;
--- router.js
+++ router.js
@@ -1,12 +1,16 @@
 let express = require('express');
-
+let passport = require('passport')
 //requiring our controller files
 let HomeController = require('./controllers/home');
+//Auth
+let UserController = require('./controllers/user');
 
 
 module.exports = function(app){
   //creating router variables
   const homeRouter = express.Router();
+  //Auth
+  const userRouter = express.Router();
 
 
   homeRouter.get('/', HomeController.index);
@@ -14,5 +18,16 @@ module.exports = function(app){
   homeRouter.get('/jobseekers', HomeController.jobseekers);
   homeRouter.get('/employed', HomeController.employed);
 
+  //authentification routes
+  userRouter.get('/login', UserController.login);
+  userRouter.get('/signup', UserController.signup);
+
+  userRouter.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login/',
+    failureFlash: true
+}))
+
   app.use('/', homeRouter);
+  app.use('/', userRouter);
 }
